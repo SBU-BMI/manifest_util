d053f491b6d4f93ed7136226bce666e1ad13d296
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-11-01 09:53:15 -0400

    Increased efficiency

--- README.md
+++ README.md
@@ -7,15 +7,9 @@ PURPOSE: FIND match in PathDB httplinks.csv, and CREATE manifest.csv
 1) Download httplinks.csv from PathDB
 Click the Collections tab, then go to the collection, and there will be a link that says "download".
 
-2) `ls -l | awk '{print $9}' > ~/myList.list`
+2) Run `python manifest_file_generator.py "/path/to/upload_dir" "/path/to/httplinks.csv" manifest_type substring_to_replace > manifest.csv`
 
-3) Run `python manifest_file_generator.py "/path/to/myList.list" "/path/to/httplinks.csv" manifest_type substring_to_replace > manifest.csv`
+What this does is it looks through your directory of things-to-upload, and it uses each "thing" as a search string to find a matching entry in httplinks.csv
 
-What this does is it looks through your file of things-to-upload,
-and it uses the "thing" as a search string to search for that entry
-in httplinks.csv
-
-If there is a part of the filename that needs to be stripped out
-(for example, file ends in "-test")
-then pass "-test" as the last argument.
-"-test" will be stripped out of the input file name so we can find its match in httplinks.csv
+If there is a part of the filename that needs to be stripped out (for example, file ends in "-test") then pass "-test" as the last argument.<br>
+"-test" will be stripped out of the search string so we can find its match in httplinks.csv
--- manifest_file_generator.py
+++ manifest_file_generator.py
@@ -4,15 +4,26 @@ import sys
 
 def main(myargs):
     arrlen = len(myargs)
-    my_list = myargs[0]
+    my_dir = myargs[0]
     image_list = myargs[1]
     manifest_type = myargs[2]
+    replace_str = ''
     if arrlen == 4:
         replace_str = myargs[3]
 
-    if not os.path.isfile(my_list):
-        print("File path {} does not exist. Exiting...".format(my_list))
+    if not os.path.exists(my_dir):
+        print("File path {} does not exist. Exiting...".format(my_dir))
         sys.exit()
+
+    my_list = [x for x in os.listdir(my_dir)]
+    length = len(my_list)
+    if length == 0:
+        print("No files in folder {}".format(my_dir))
+        sys.exit()
+
+    # if not os.path.isfile(my_list):
+    #     print("File path {} does not exist. Exiting...".format(my_list))
+    #     sys.exit()
 
     if not os.path.isfile(image_list):
         print("File path {} does not exist. Exiting...".format(my_list))
@@ -28,26 +39,44 @@ def main(myargs):
         if manifest_type == 'image':
             print('path,studyid,clinicaltrialsubjectid,imageid')
 
-        with open(my_list) as fa:
-            for line_a in fa:
-                # Ignore blank lines
-                if len(line_a.strip()) > 0:
-                    if replace_str:
-                        line_a = line_a.replace(replace_str, "")
-                    with open(image_list) as fb:
-                        next(fb)  # Skip header row
-                        for line_b in fb:
-                            key = line_a[:line_a.find(".")].strip()
-                            val = line_b[:line_b.find(".")].strip()
-                            row = line_b.strip().split(',')
-                            if key in val:
-                                if manifest_type == 'map':
-                                    print(row[1] + "," + row[2] + "," + row[3] + "," + line_a.strip())
-                                if manifest_type == 'segmentation':
-                                    print(line_a.strip() + "," + row[1] + "," + row[2] + "," + row[3])
-                                if manifest_type == 'image':
-                                    print(row[1] + "," + row[2] + "," + row[3] + "," + line_a.strip())
-                                continue
+        for line_a in my_list:
+            if replace_str:
+                line_a = line_a.replace(replace_str, "")
+            with open(image_list) as fb:
+                next(fb)  # Skip header row
+                for line_b in fb:
+                    key = line_a[:line_a.find(".")].strip()
+                    val = line_b[:line_b.find(".")].strip()
+                    row = line_b.strip().split(',')
+                    if key in val:
+                        if manifest_type == 'map':
+                            print(row[1] + "," + row[2] + "," + row[3] + "," + line_a.strip())
+                        if manifest_type == 'segmentation':
+                            print(line_a.strip() + "," + row[1] + "," + row[2] + "," + row[3])
+                        if manifest_type == 'image':
+                            print(row[1] + "," + row[2] + "," + row[3] + "," + line_a.strip())
+                        continue
+
+        # with open(my_list) as fa:
+        #     for line_a in fa:
+        #         # Ignore blank lines
+        #         if len(line_a.strip()) > 0:
+        #             if replace_str:
+        #                 line_a = line_a.replace(replace_str, "")
+        #             with open(image_list) as fb:
+        #                 next(fb)  # Skip header row
+        #                 for line_b in fb:
+        #                     key = line_a[:line_a.find(".")].strip()
+        #                     val = line_b[:line_b.find(".")].strip()
+        #                     row = line_b.strip().split(',')
+        #                     if key in val:
+        #                         if manifest_type == 'map':
+        #                             print(row[1] + "," + row[2] + "," + row[3] + "," + line_a.strip())
+        #                         if manifest_type == 'segmentation':
+        #                             print(line_a.strip() + "," + row[1] + "," + row[2] + "," + row[3])
+        #                         if manifest_type == 'image':
+        #                             print(row[1] + "," + row[2] + "," + row[3] + "," + line_a.strip())
+        #                         continue
     except Exception as ex:
         print(ex)
         sys.exit(1)
@@ -55,9 +84,9 @@ def main(myargs):
 
 if __name__ == '__main__':
     # 1) Download httplinks.csv from PathDB
-    # 2) ls -l | awk '{print $9}' > ~/myList.list
     if len(sys.argv) < 4:
         print('\nUSAGE:\n    python ' + os.path.basename(
-            __file__) + ' /path/to/yourImageList.list /path/to/httplinks.csv manifest_type [map | segmentation] <optional: substring_to_replace>')
+            __file__) + ' /path/to/upload_dir /path/to/httplinks.csv manifest_type [map | segmentation] '
+              + '<optional: substring_to_replace>')
     else:
         main(sys.argv[1:])
