0b61364a1c993cc2f6c224c77f89c62ee618630d
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-12-05 14:29:28 -0500

    check for dupes after creation

---
+++ check_after.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+check your manifest file after-the-fact
+"""
+import pandas as pd
+
+
+def main():
+    # making data frame from csv file
+    data = pd.read_csv('manifest.csv')
+
+    # sorting by first name
+    data.sort_values('filename', inplace=True)
+
+    # making a bool series
+    bool_series = data['filename'].duplicated(keep=False)  # False means consider all of the same values as duplicates
+
+    # displaying data
+    # print(data.head())
+
+    # display data
+    x = data[bool_series]
+    if len(x) == 0:
+        print('No duplicate filenames')
+    else:
+        print(x)
+        print('DUPLICATE FILENAMES FOUND!!')
+
+
+if __name__ == '__main__':
+    main()
