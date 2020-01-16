ddd6029811465ac59d92ee40eb826c442b6c1d6c
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-01-16 11:35:21 -0500

    update

--- temp/in_case_of_mess.sh
+++ temp/in_case_of_mess.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 # "Mess" example:
-# Someone says: I mixed the results files together, good luck figuring it out. :\
+# Someone says: I mixed the results together, good luck figuring it out. :\
 
-concat_csvs () {
-  echo "Help is on the way! :)"
+echo "Help is on the way! :)"
 
-  USER="$1"
-  # Will request password:
-  TOKEN=$(curl -u $USER http://localhost/jwt/token | python -c "import sys, json; print json.load(sys.stdin)['token']")
-  # Download SEER:Rutgers:Lung
-  curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" -o seer_rutgers_lung.csv http://localhost/csvdownload/3
-  # Download SEER:Rutgers:Prostate
-  curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" -o seer_rutgers_prostate.csv http://localhost/csvdownload/4
-  cp seer_rutgers_prostate.csv temp.txt
-  # Remove first line
-  sed '1d' temp.txt > tmpfile; mv tmpfile temp.csv
-  # Concatenate
-  cat seer_rutgers_lung.csv temp.csv > lung_and_prostate.csv
-  # rm "temp.*"
-  echo "CONCATENATED CSV: lung_and_prostate.csv"
-}
-concat_csvs
+USER="$1"
+# Will request password:
+TOKEN=$(curl -u $USER http://localhost/jwt/token | python -c "import sys, json; print json.load(sys.stdin)['token']")
+# Download SEER:Rutgers:Lung
+curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" -o seer_rutgers_lung.csv http://localhost/csvdownload/3
+# Download SEER:Rutgers:Prostate
+curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" -o seer_rutgers_prostate.csv http://localhost/csvdownload/4
+cp seer_rutgers_prostate.csv temp.txt
+# Remove first line
+sed '1d' temp.txt > tmpfile
+mv tmpfile temp.csv
+# Concatenate
+cat seer_rutgers_lung.csv temp.csv > lung_and_prostate.csv
+# rm "temp.*"
+echo "CONCATENATED CSV: lung_and_prostate.csv"
 
 ./crt_man.sh
